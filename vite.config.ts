75af85f3163243b9b2d6625221c4736322bdbe76
Jon Laing <jon@farsight-ai.com>
2025-12-06 22:55:49 -0500

    clean up so that it's a proper package

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
-// vite.config.ts
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "src/main.ts"),
-      name: "Effect.UI",
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "EffectUI",
       fileName: "effect-ui",
     },
+    emptyOutDir: false,
     rollupOptions: {
-      external: [],
+      external: ["effect"],
       output: {
-        globals: {},
+        globals: {
+          effect: "Effect",
+        },
       },
     },
   },
