7a2844fab7fe98800785f9c58f89cd04b8003bee
Jon Laing <jon@farsight-ai.com>
2026-01-27 01:27:52 -0500

    clean up exports a little

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,59 +1,28 @@
 // Readable - exports both the interface type and namespace (declaration merging)
-export {
-  Readable,
-  TypeId as ReadableTypeId,
-  type Reactive,
-  isReadable,
-  of,
-  id,
-  normalize,
-  make as makeReadable,
-  map as mapReadable,
-  flatMap as flatMapReadable,
-  zip as zipReadable,
-  zipWith as zipWithReadable,
-  zipAll,
-  combine,
-  tap as tapReadable,
-  filter as filterReadable,
-  dedupe,
-  dedupeWith,
-  fromStream,
-  lift,
-} from "./Readable";
+export { Readable, TypeId as ReadableTypeId, type Reactive } from "./Readable";
 
 // Signal - exports both the interface type and namespace (declaration merging)
 export {
   Signal,
   SignalTypeId,
-  isSignal,
-  equals as equalsSignal,
-  type SignalOptions,
   SignalRegistry,
   make as makeSignal,
-  fromNullable,
-  fromReactive,
   type SignalArray,
   type SignalMap,
   type SignalSet,
 } from "./Signal";
 
 // AsyncReadable
-export {
-  AsyncReadable,
-  AsyncReadableTypeId,
-  isAsyncReadable,
-} from "./AsyncReadable";
+export { AsyncReadable, AsyncReadableTypeId } from "./AsyncReadable";
 
 // Mutation
-export { Mutation, MutationTypeId, isMutation } from "./Mutation";
+export { Mutation, MutationTypeId } from "./Mutation";
 
 // Ref
-export type { Ref as RefType } from "./Ref";
 export { Ref } from "./Ref";
 
 // Renderer
-export type { Renderer as RendererInterface, Slot } from "./Renderer";
+export type { Slot } from "./Renderer";
 export { Renderer, RendererContext } from "./Renderer";
 
 // Element
@@ -61,11 +30,10 @@ export type { Element, Child } from "./Element";
 
 // Boundary
 export type { SuspenseOptions, BoundarySuspenseOptions } from "./Boundary";
-export { Boundary, suspense, error } from "./Boundary";
+export { Boundary } from "./Boundary";
 
 // Transition
 export type {
-  Transition as TransitionType,
   TransitionConfig,
   TransitionTarget,
   GuardedTarget,
