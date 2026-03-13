1061803ade43b9ae8b18cb339ddf2dd890dd5f54
Jon Laing <jon@farsight-ai.com>
2026-03-13 01:24:51 -0400

    get rid of cli and eslint-plugin packages, they're gratuitous

--- packages/eslint-plugin/src/lib/createRule.ts
+++
@@ -1,6 +0,0 @@
-import { ESLintUtils } from "@typescript-eslint/utils";
-
-export const createRule = ESLintUtils.RuleCreator(
-  (name) =>
-    `https://github.com/jonlaing/effex/tree/main/packages/eslint-plugin/docs/${name}.md`,
-);
--- packages/eslint-plugin/src/rules/index.ts
+++
@@ -1,5 +0,0 @@
-export { noThrow } from "./no-throw";
-export { noTryCatch } from "./no-try-catch";
-export { noNull } from "./no-null";
-export { noFloatingEffect } from "./no-floating-effect";
-export { preferTemplateLiteral } from "./prefer-template-literal";
